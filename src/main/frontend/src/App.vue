44d0eab03a016b1ad48a14902e30bdbc8b98a449
student16 <student16@pcoip.ki.agh.edu.pl>
2018-06-09 14:23:17 +0200

    polaczenie z baza

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -27,37 +27,47 @@
 </template>
 
 <script>
-    import "milligram";
-    import LoginForm from "./LoginForm";
-    import MeetingsPage from "./meetings/MeetingsPage";
+import "milligram";
+import LoginForm from "./LoginForm";
+import MeetingsPage from "./meetings/MeetingsPage";
 
-    export default {
-        components: {LoginForm, MeetingsPage},
-        data() {
-            return {
-                authenticatedUsername: "",
-                isRegistering: false
-            };
-        },
-        methods: {
-            login(user) {
-                this.authenticatedUsername = user.login;
-            },
-            logout() {
-                this.authenticatedUsername = '';
-            }
-        }
+export default {
+  components: { LoginForm, MeetingsPage },
+  data() {
+    return {
+      authenticatedUsername: "",
+      isRegistering: false
     };
+  },
+  methods: {
+    login(user) {
+      this.authenticatedUsername = user.login;
+    },
+    logout() {
+      this.authenticatedUsername = "";
+    },
+    register(user) {
+      this.$http
+        .post("participants", user)
+        .then(response => {
+          // udało się
+        })
+        .catch(response => {
+          // nie udało sie
+        });
+    }
+  }
+};
 </script>
 
 <style>
-  #app {
-    max-width: 1000px;
-    margin: 0 auto;
-  }
+#app {
+  max-width: 1000px;
+  margin: 0 auto;
+}
 
-  .logo {
-    vertical-align: middle;
-  }
+.logo {
+  vertical-align: middle;
+}
 </style>
 
